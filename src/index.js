b9fbd2cf24b6b6eee027a4f5107d873d057b2b20
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-04-15 11:39:23 +0100

    Fixing React version error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root');
+//root.render(
+render(  
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want your app to work offline and load faster, you can change
