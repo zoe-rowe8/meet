2d629334e61e6d25cc5ee8df9be3c279bdbbeb54
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-04-28 00:21:36 +0100

    Task 4.7: Continuous Delivery

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('fe58d569fd914ba4a61e5f862edcef62').install();
 
 ReactDOM.render(
   <React.StrictMode>
