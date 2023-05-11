a165f416358ad7acd7cedce2f6f402d7d29425c5
zoe-rowe8 <zoemarierowe@hotmail.co.uk>
2023-05-11 11:33:33 +0100

    Adding GetData

--- src/App.js
+++ src/App.js
@@ -49,6 +49,16 @@ class App extends Component {
     });
     }
 
+    getData = () => {
+      const {locations, events} = this.state;
+      const data = locations.map((location)=>{
+        const number = events.filter((event) => event.location === location).length
+        const city = location.split(', ').shift()
+        return {city, number};
+      })
+      return data;
+    };
+
   render() {
     return (
       <div className="App">
